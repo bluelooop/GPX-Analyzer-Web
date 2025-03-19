a87b7fc9d956d644a3e202328c952a51d6994d8c
Santiago de Pedro <santiago.pedro23@gmail.com>
2025-03-19 17:21:06 -0400

    Update dependencies in yarn.lock with new package versions
    This commit updates `yarn.lock` with multiple new or upgraded dependencies, including packages like `@babel/runtime`, `@parcel/watcher`, `@fluentui/react`, and others. These changes ensure compatibility, address potential bugs, and improve package security and stability.

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Homepage from './pages/Homepage/Homepage.tsx';
-
+import 'semantic-ui-css/semantic.min.css';
 
 const appRoot = document.getElementById('app')!;
 
