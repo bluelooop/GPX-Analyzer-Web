bee1a84e7d7bf48e44f03b8a9754ba4ed26faf85
Santiago de Pedro <santiago.pedro23@gmail.com>
2025-03-20 19:15:08 -0400

    Reorder middleware initialization and loading logic
    Moved `loadConfigurations` to execute before middleware setup. This ensures configurations are loaded prior to server initialization, improving clarity and potential configuration handling.

--- api/server.ts
+++ api/server.ts
@@ -6,6 +6,18 @@ import cors from 'cors';
 
 const server: express.Application = express();
 
+loadConfigurations();
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true, // Allow cookies and credentials
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11) choke on 204
+};
+
+server.use(cors(corsOptions));
+
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(cookieParser());
@@ -14,18 +26,6 @@ server.use(cookieParser());
 server.use('/api', apiRouter);
 
 server.listen(3000, () => {
-  loadConfigurations();
-
-  const corsOptions = {
-    origin: process.env.FRONTEND_URL,
-    credentials: true, // Allow cookies and credentials
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-    optionsSuccessStatus: 200, // Some legacy browsers (IE11) choke on 204
-  };
-
-  server.use(cors(corsOptions));
-
   console.log('Server is running on port 3000');
   console.log('http://localhost:3000/api');
 });
