08feae3316f4b8e833cdecd68fc768dc93aed988
Santiago de Pedro <santiago.pedro23@gmail.com>
2025-03-20 19:08:13 -0400

    Add cookie-parser and CORS middleware to server
    Integrated cookie-parser and configured CORS to enhance server compatibility with front-end requests and support cross-origin resource sharing. Updated dependencies in package.json and yarn.lock to include necessary packages.

--- api/server.ts
+++ api/server.ts
@@ -1,17 +1,30 @@
 import express from 'express';
 import { apiRouter } from './apps/routes';
 import { loadConfigurations } from './configurations';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const server: express.Application = express();
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+server.use(cookieParser());
 
 // Specify all routes
 server.use('/api', apiRouter);
 
 server.listen(3000, () => {
   loadConfigurations();
+
+  const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    credentials: true, // Allow cookies and credentials
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    optionsSuccessStatus: 200, // Some legacy browsers (IE11) choke on 204
+  };
+
+  server.use(cors(corsOptions));
 
   console.log('Server is running on port 3000');
   console.log('http://localhost:3000/api');
