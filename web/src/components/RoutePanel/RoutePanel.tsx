aafbd5d4d43814dd0a54a98a736bae6d749f7a64
Santiago de Pedro <santiago.pedro23@gmail.com>
2025-03-20 23:44:15 -0400

    Add SegmentsPanel and enhance RoutePanel with detailed stats
    Introduce a SegmentsPanel component to display route segment data in a structured table. Update RoutePanel to include route details (name, description, stats) and integrate SegmentsPanel. Add basic styling for RoutePanel to improve layout and readability.

--- web/src/components/RoutePanel/RoutePanel.tsx
+++ web/src/components/RoutePanel/RoutePanel.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import { GPXRoute } from '../../models.ts';
+import { Grid } from 'semantic-ui-react';
 
-const RoutePanel: React.FC = () => {
+import './RoutePanel.scss';
+import SegmentsPanel from './SegmentsPanel.tsx';
+
+interface RoutePanelProps {
+  route: GPXRoute;
+}
+
+const RoutePanel: React.FC<RoutePanelProps> = ({ route }) => {
   return (
     <div className="route-panel">
-      {/* Add your content or logic here */}
-      <p>Route panel Component</p>
+      <Grid stackable>
+        <Grid.Row>
+          <Grid.Column>
+            <h1>{route.name}</h1>
+          </Grid.Column>
+        </Grid.Row>
+        {route.description && (
+          <Grid.Row>
+            <Grid.Column>
+              <h1>{route.description}</h1>
+            </Grid.Column>
+          </Grid.Row>
+        )}
+        <Grid.Row>
+          <Grid.Column>
+            <div className="route-stats">
+              <span>
+                <b>Distance</b>: {route.distance.toFixed(2)} km
+              </span>
+              <span>
+                <b>D+</b>: {route.elevationGain.toFixed(2)} m
+              </span>
+              <span>
+                <b>D-</b>: {route.elevationLoss.toFixed(2)} m
+              </span>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <SegmentsPanel segments={route.segments} />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   );
 };
---
+++ web/src/components/RoutePanel/SegmentsPanel.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { GPXSegment } from '../../models.ts';
+import {
+  Header,
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableHeaderCell,
+  TableRow,
+} from 'semantic-ui-react';
+
+interface SegmentsPanelProps {
+  segments: GPXSegment[];
+}
+
+const SegmentsPanel: React.FC<SegmentsPanelProps> = ({ segments }) => {
+  return (
+    <Table celled striped structured color={'blue'}>
+      <TableHeader>
+        <TableRow>
+          <TableHeaderCell rowSpan={2} textAlign="center">
+            <b>Number</b>
+          </TableHeaderCell>
+          <TableHeaderCell colSpan={4} textAlign="center">
+            <b>Elevations</b>
+          </TableHeaderCell>
+          <TableHeaderCell colSpan={3} textAlign="center">
+            <b>Distances</b>
+          </TableHeaderCell>
+          <TableHeaderCell rowSpan={2} textAlign="center">
+            <b>D+</b>
+          </TableHeaderCell>
+          <TableHeaderCell rowSpan={2} textAlign="center">
+            <b>D-</b>
+          </TableHeaderCell>
+          <TableHeaderCell colSpan={3} textAlign="center">
+            <b>Grades</b>
+          </TableHeaderCell>
+        </TableRow>
+        <TableRow>
+          <TableHeaderCell textAlign="center">
+            <b>Start</b>
+          </TableHeaderCell>
+          <TableHeaderCell textAlign="center">
+            <b>End</b>
+          </TableHeaderCell>
+          <TableHeaderCell textAlign="center">
+            <b>Min</b>
+          </TableHeaderCell>
+          <TableHeaderCell textAlign="center">
+            <b>Max</b>
+          </TableHeaderCell>
+          <TableHeaderCell textAlign="center">
+            <b>Total</b>
+          </TableHeaderCell>
+          <TableHeaderCell textAlign="center">
+            <b>Start</b>
+          </TableHeaderCell>
+          <TableHeaderCell textAlign="center">
+            <b>End</b>
+          </TableHeaderCell>
+          <TableHeaderCell textAlign="center">
+            <b>Min</b>
+          </TableHeaderCell>
+          <TableHeaderCell textAlign="center">
+            <b>Avg</b>
+          </TableHeaderCell>
+          <TableHeaderCell textAlign="center">
+            <b>Max</b>
+          </TableHeaderCell>
+        </TableRow>
+      </TableHeader>
+
+      <TableBody>
+        {segments.map((segment) => (
+          <TableRow key={segment.number}>
+            <TableCell>
+              <Header as="h3" textAlign="center">
+                {segment.number}
+              </Header>
+            </TableCell>
+            <TableCell>{segment.startElevation.toFixed(2)} m</TableCell>
+            <TableCell>{segment.endElevation.toFixed(2)} m</TableCell>
+            <TableCell>{segment.minElevation.toFixed(2)} m</TableCell>
+            <TableCell>{segment.maxElevation.toFixed(2)} m</TableCell>
+            <TableCell>{segment.distance.toFixed(2)} m</TableCell>
+            <TableCell>{segment.startDistance.toFixed(2)} m</TableCell>
+            <TableCell>{segment.endDistance.toFixed(2)} m</TableCell>
+            <TableCell>{segment.elevationGain.toFixed(2)} m</TableCell>
+            <TableCell>{segment.elevationLoss.toFixed(2)} m</TableCell>
+            <TableCell>{segment.minGrade.toFixed(2)} m</TableCell>
+            <TableCell>{segment.avgGrade.toFixed(2)} m</TableCell>
+            <TableCell>{segment.maxGrade.toFixed(2)} m</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default SegmentsPanel;
