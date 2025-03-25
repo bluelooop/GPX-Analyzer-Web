8c4a4ec53020b772e9406911c4d03ffc9c29a807
Santiago de Pedro <santiago.pedro23@gmail.com>
2025-03-25 17:55:13 -0400

    Simplify cookie settings in auth views
    Removed the conditional `secure` flag setting for cookies. This ensures consistent handling of cookies and reduces complexity in the authentication flow.

--- api/apps/auth/views.ts
+++ api/apps/auth/views.ts
@@ -50,7 +50,6 @@ authRouter.get(
       res.cookie('_rpat', accessToken.tokens.accessToken, {
         httpOnly: true,
         expires: new Date(Date.now() + accessToken.tokens.expiresIn * 1000),
-        secure: redirectURL.protocol === 'https:',
       });
 
       return res.redirect(`${redirectURL.toString()}?auth=${accessToken.tokens.expiresIn}`);
