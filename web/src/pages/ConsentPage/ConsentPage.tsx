d0b0eb0b636cbe5bd1c370aae5a5728a0325cca5
Santiago de Pedro <santiago.pedro23@gmail.com>
2025-03-26 11:47:15 -0400

    Validate authentication and improve error handling logic.
    Refines the authentication check by ensuring the `auth` parameter is explicitly validated against `'true'`. Adds logging for debugging purposes and enhances safety by verifying required parameters before proceeding. The error handling and window closing logic remain consistent but clearer.

--- web/src/pages/ConsentPage/ConsentPage.tsx
+++ web/src/pages/ConsentPage/ConsentPage.tsx
@@ -8,23 +8,25 @@ const ConsentPage: React.FC = () => {
 
   useEffect(() => {
     const authenticated = searchParams['auth'];
-    if (authenticated) {
+    if (authenticated && authenticated === 'true') {
       const code = searchParams['code'];
       const expires = searchParams['expires'];
       const expiresNumber = parseInt(expires, 10);
-      if (expiresNumber) {
+      console.log(code, expiresNumber);
+      if (code && expiresNumber) {
         setCookie('_rpa', '1', expiresNumber);
         setCookie('_rpat', code, expiresNumber, true, location.protocol === 'https:');
         window.close();
       }
-      setConsentError(
-        'Something went wrong or you cancel the consent request. Please try again later.',
-      );
-
-      setTimeout(() => {
-        window.close();
-      }, 3000);
     }
+
+    setConsentError(
+      'Something went wrong or you cancel the consent request. Please try again later.',
+    );
+
+    setTimeout(() => {
+      window.close();
+    }, 3000);
   }, [searchParams]);
 
   return <div>{consentError && consentError}</div>;
