02a0c7090affd3367f068608baf3629b1472770d
Santiago de Pedro <santiago.pedro23@gmail.com>
2025-03-27 15:13:34 -0400

    Update CORS handling to validate request origins dynamically
    Replaced static origin array with a dynamic validation function. This allows more precise control by checking each request's origin against allowed origins. Unrecognized origins now produce an error, enhancing security and flexibility.

--- api/server.ts
+++ api/server.ts
@@ -9,7 +9,23 @@ const server: express.Application = express();
 loadConfigurations();
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URLS.split(',').map((url) => url.trim()),
+  origin: (
+    requestOrigin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void,
+  ): void => {
+    const allowedOrigins = process.env.FRONTEND_URLS.split(',').map((url) => url.trim()) || [];
+
+    if (!requestOrigin) {
+      callback(null, true);
+      return;
+    }
+
+    if (allowedOrigins.includes(requestOrigin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true, // Allow cookies and credentials
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
